5f2fecd23aa390a17963bbbb2bdf31d752e18c50
f-fernando-f <fernandesfernando1000@yahoo.com.br>
2019-11-08 16:37:50 -0300

    Validacao Firebird.

--- src/main/java/br/com/aurum/scriptFormat/enums/ValidacoesFirebird.java
+++ src/main/java/br/com/aurum/scriptFormat/enums/ValidacoesFirebird.java
@@ -1,7 +1,95 @@
 package br.com.aurum.scriptFormat.enums;
 
 public enum ValidacoesFirebird {
-	INSERT_INTO("");
+	INSERT_INTO("execute block\n" + 
+			"as\n" + 
+			"declare chk int;\n" + 
+			"BEGIN\n" + 
+			" SELECT count(*) from %s WHERE %s = %s INTO :chk;    \n" + 
+			" if (chk = 0 ) then\n" + 
+			" begin\n" + 
+			"  	%s\n" + 
+			" end\n" + 
+			"END"),
+	CREATE_TABLE("execute block\n" + 
+			"as\n" + 
+			"declare chk int;\n" + 
+			"BEGIN\n" + 
+			" SELECT count(*) from RDB$RELATIONS WHERE RDB$RELATION_NAME = '%s' INTO :chk;    \n" + 
+			" if (chk = 0 ) then\n" + 
+			" begin\n" + 
+			"  	%s\n" + 
+			" end\n" + 
+			"END"),
+	ADD_COLUMN("execute block\n" + 
+			"as\n" + 
+			"declare chk int;\n" + 
+			"BEGIN\n" + 
+			" SELECT count(*) FROM RDB$RELATION_FIELDS WHERE RDB$FIELD_NAME = '%s' AND RDB$RELATION_NAME = '%s' INTO :chk;    \n" + 
+			" if (chk = 0 ) then\n" + 
+			" begin\n" + 
+			"  	%s\n" + 
+			" end\n" + 
+			"END"),
+	ADD_FOREIGN_KEY("execute block\n" + 
+			"as\n" + 
+			"declare chk int;\n" + 
+			"BEGIN\n" + 
+			" SELECT count(*execute block\n" + 
+			"as\n" + 
+			"declare chk int;\n" + 
+			"BEGIN\n" + 
+			" SELECT count(*) FROM RDB$RELATION_CONSTRAINTS WHERE RDB$CONSTRAINT_NAME = '%s' INTO :chk;    \n" + 
+			" if (chk = 0 ) then\n" + 
+			" begin\n" + 
+			"  	%s\n" + 
+			" end\n" + 
+			"END) FROM RDB$RELATION_CONSTRAINTS WHERE RDB$CONSTRAINT_NAME = '%s' INTO :chk;    \n" + 
+			" if (chk = 0 ) then\n" + 
+			" begin\n" + 
+			"  	%s\n" + 
+			" end\n" + 
+			"END"),
+	DROP_TABLE("execute block\n" + 
+			"as\n" + 
+			"declare chk int;\n" + 
+			"BEGIN\n" + 
+			" SELECT count(*) from RDB$RELATIONS WHERE RDB$RELATION_NAME = '%s' INTO :chk;    \n" + 
+			" if (chk = 1 ) then\n" + 
+			" begin\n" + 
+			"  	%s\n" + 
+			" end\n" + 
+			"END"),
+	DROP_COLUMN("execute block\n" + 
+			"as\n" + 
+			"declare chk int;\n" + 
+			"BEGIN\n" + 
+			" SELECT count(*) FROM RDB$RELATION_FIELDS WHERE RDB$FIELD_NAME = '%s' AND RDB$RELATION_NAME = '%s' INTO :chk;    \n" + 
+			" if (chk = 1 ) then\n" + 
+			" begin\n" + 
+			"  	%s\n" + 
+			" end\n" + 
+			"END"),
+	DROP_CONSTRAINT("execute block\n" + 
+			"as\n" + 
+			"declare chk int;\n" + 
+			"BEGIN\n" + 
+			" SELECT count(*) FROM RDB$RELATION_CONSTRAINTS WHERE RDB$CONSTRAINT_NAME = '%s' INTO :chk;    \n" + 
+			" if (chk = 1 ) then\n" + 
+			" begin\n" + 
+			"  	%s\n" + 
+			" end\n" + 
+			"END"),
+	ADD_PRIMARY_KEY("execute block\n" + 
+			"as\n" + 
+			"declare chk int;\n" + 
+			"BEGIN\n" + 
+			" SELECT count(*) FROM RDB$RELATION_CONSTRAINTS WHERE RDB$CONSTRAINT_NAME = '%s' INTO :chk;    \n" + 
+			" if (chk = 0 ) then\n" + 
+			" begin\n" + 
+			"  	%s\n" + 
+			" end\n" + 
+			"END");
 	
 	private String valor;
 
