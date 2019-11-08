5ac556ad557cad314a2afe776a2291f74cc9ffd2
gabriel arseno <gabrielarsgal@gmail.com>
2019-11-08 16:56:53 -0300

    corrigindo replacer

--- src/main/java/br/com/aurum/scriptFormat/rest/InsertRest.java
+++ src/main/java/br/com/aurum/scriptFormat/rest/InsertRest.java
@@ -1,24 +1,30 @@
 package br.com.aurum.scriptFormat.rest;
 
+import br.com.aurum.scriptFormat.enums.ValidacoesOracle;
+import br.com.aurum.scriptFormat.enums.ValidacoesSQL;
+import br.com.aurum.scriptFormat.helper.Replacer;
+import br.com.aurum.scriptFormat.model.Insert;
+import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.gson.Gson;
-
-import br.com.aurum.scriptFormat.enums.ValidacoesOracle;
-import br.com.aurum.scriptFormat.enums.ValidacoesSQL;
-import br.com.aurum.scriptFormat.model.Insert;
-
 @RestController
 public class InsertRest {
-	
+
+
+
 	@RequestMapping(value="/insert")
-	public @ResponseBody String getInsert(@RequestParam String query, @RequestParam String table, @RequestParam String column, @RequestParam String value, @RequestParam Integer number) {
+	public @ResponseBody String getInsert(@RequestParam String query, @RequestParam String table, @RequestParam String column,
+										  @RequestParam String value, @RequestParam Integer number) {
+
 		Insert insert = new Insert().withQuery(query).withTable(table).withColumn(column).havingValue(value).withComment(number);
-		insert.setSqlServer(String.format(ValidacoesSQL.INSERT_INTO.getValor(), table, column, value, query));
-		insert.setOracle(String.format(ValidacoesOracle.INSERT_INTO.getValor(), table, column, value, query));
+		Replacer replacer = new Replacer();
+		insert.setSqlServer(String.format(ValidacoesSQL.INSERT_INTO.getValor(), table, column, value,
+				replacer.replaceQueryToSqlServer(query.toUpperCase())));
+		insert.setOracle(String.format(ValidacoesOracle.INSERT_INTO.getValor(), table, column, value,
+				replacer.replaceQueryToOracle(query.toUpperCase())));
 		Gson gson = new Gson();
 		return gson.toJson(insert);
 	}
